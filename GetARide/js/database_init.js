7f5134654fa6b9900c79ee0714557df88553a4a7
Dan Tiberi <dtiberi@hawk.iit.edu>
2021-05-03 16:25:42 -0500

    Tweaked db + crud. Various new crud functions.

--- GetARide/js/database_init.js
+++ GetARide/js/database_init.js
@@ -18,9 +18,9 @@ window.onload = function() {
 
     //Create tables
     alasql("DROP TABLE IF EXISTS users, drivers, ride, payment_info, vehicles"); //Remove existing tables. 
-    alasql("CREATE TABLE IF NOT EXISTS users (user_id INT NOT NULL AUTO_INCREMENT, email VARCHAR(50), password VARCHAR(50), fname VARCHAR(30), lname VARCHAR(30), PRIMARY KEY (user_id))");
-    alasql("CREATE TABLE IF NOT EXISTS drivers (driver_id INT NOT NULL AUTO_INCREMENT, email VARCHAR(50), password VARCHAR(50), fname VARCHAR(30), lname VARCHAR(30), is_avail BOOLEAN, avg_rating DOUBLE, num_trips INT, routing_number VARCHAR(30), last_background_check DATETIME, PRIMARY KEY (driver_id))");
-    alasql("CREATE TABLE IF NOT EXISTS rides (ride_id INT NOT NULL AUTO_INCREMENT, passenger_id INT, driver_id INT, vehicle_id INT, fee DOUBLE, tax DOUBLE, origin VARCHAR(50), destination VARCHAR(50), start_time DATETIME, end_time DATETIME, user_rating INT, payment_card_id INT, status BOOLEAN, PRIMARY KEY (ride_id))");
+    alasql("CREATE TABLE IF NOT EXISTS users (user_id INT NOT NULL AUTO_INCREMENT, email VARCHAR(50), password VARCHAR(50), fname VARCHAR(30), lname VARCHAR(30), phone_number VARCHAR(20), PRIMARY KEY (user_id))");
+    alasql("CREATE TABLE IF NOT EXISTS drivers (driver_id INT NOT NULL AUTO_INCREMENT, email VARCHAR(50), password VARCHAR(50), fname VARCHAR(30), lname VARCHAR(30), is_avail BOOLEAN, avg_rating DOUBLE, num_trips INT, routing_number VARCHAR(30), last_background_check DATETIME, status BOOLEAN, phone_number VARCHAR(20), PRIMARY KEY (driver_id))");
+    alasql("CREATE TABLE IF NOT EXISTS rides (ride_id INT NOT NULL AUTO_INCREMENT, passenger_id INT, driver_id INT, vehicle_id INT, fee DOUBLE, tax DOUBLE, origin VARCHAR(50), destination VARCHAR(50), start_time DATETIME, duration TIME, user_rating INT, payment_card_id INT, status ENUM(requested, taken, complete), PRIMARY KEY (ride_id))");
     alasql("CREATE TABLE IF NOT EXISTS payment_info (card_id INT NOT NULL AUTO_INCREMENT, user_id INT, card_number VARCHAR(20), card_cvv INT, card_type VARCHAR(20), PRIMARY KEY (card_id))");
     alasql("CREATE TABLE IF NOT EXISTS vehicles (vehicle_id INT NOT NULL AUTO_INCREMENT, driver_id INT, lplate_num VARCHAR(20), model_year YEAR, vehicle_make VARCHAR(20), vehicle_model VARCHAR(20), passed_qa BOOLEAN, pas_capacity INT, quality_rating INT, PRIMARY KEY (vehicle_id))");
     populate(true); //If true, will populate databse with dummy information for demo or testing purposes. 
@@ -36,7 +36,7 @@ function populate(tog){
         addDummyDriver(true);
         addDummyDriver(true);
 
-        newUser("Dt@gmail.com", "abc123", "Bob", "Bobbyson", "1947-2-15");
+        newUser("dt@gmail.com", "abc123", "Bob", "Bobbyson", "1947-2-15", "222-222-2222");
 
         newVehicle(2,"OutATime", 2015, "Toyota", "Prius", true, 3, 5);
         newVehicle(2,"Got2GoFast", 2019, "Ford", "Fusion", true, 3, 5);
@@ -52,14 +52,20 @@ function populate(tog){
 
         newPaymentMethod(4, "1234 5678 9101", 223, "Visa")
 
-        setAllRidesStatus(false);
+        //console.log(getRideStatus(2));
+        //setRideStatus(2, "taken")
+        //console.log(getRideStatus(2));
+
+        //console.log(getActiveRide(2))
+
+        //setDriverStatus(2, false)
 
         //setRideStatus(1, true);
         //set("users", "fname", "Obama", 1);
 
-        console.log(alasql("SELECT * FROM rides"));
+        //console.log(alasql("SELECT * FROM drivers"));
 
-        console.log("AVG Rating driver 2: " + getDriverRating(2));
+        //console.log("AVG Rating driver 2: " + getDriverRating(2));
         //console.log(retrieve("users", "fname", 1));
         //newDriver("NormalManDan@aol.com", "abc123", "Dan", "Notmehehe");
 
